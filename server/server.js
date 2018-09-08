a892068e695c2bc60587e071772e28d53dbc03f3
Fernando Renteria <rentfern@gmail.com>
2018-09-08 16:44:10 -0500

    broadcast new users joining chat

--- server/server.js
+++ server/server.js
@@ -20,12 +20,33 @@ io.on("connection", (socket) => {
     // socket.emit will emit message to single connection
     socket.on("createMessage", (message) => {
         console.log("create message", message);
+
+        // Welcome new user joining chat
+        socket.emit("newMessage", {
+            from: "Admin",
+            text: "Welcome to the chat app!",
+            createdAt: new Date().getTime()
+        });
+
+        socket.broadcast.emit("newMessage", {
+            from: "Admin",
+            text: "New user joined",
+            createdAt: new Date().getTime()
+        });
+
         // io.emit will send message to all connections
         io.emit("newMessage", {
             from: message.from,
             text: message.text,
             createdAt: new Date().getTime()
         });
+
+        // will emit message to everyone but current connection
+        // socket.broadcast.emit("newMessage", {
+        //     from: message.from,
+        //     text: message.text,
+        //     createdAt: new Date().getTime()
+        // });
     });
 
     socket.on("disconnect", () => {

20671047f30345ebca315f07d277a752fe92e0fb
Fernando Renteria <rentfern@gmail.com>
2018-09-11 09:27:52 -0500

    Started moving users to specific rooms using socket.io

--- server/server.js
+++ server/server.js
@@ -20,14 +20,16 @@ app.use(express.static(publicPath));
 io.on("connection", (socket) => {
     console.log("new connection");
 
-    // Welcome new user joining chat
-    socket.emit("newMessage", generateMessage("Admin", "Welcome to the chat app"));
-    socket.broadcast.emit("newMessage", generateMessage("Admin", "New user joined"));
-
     socket.on("join", (params, callback) => {
         if(!isRealString(params.name) || !isRealString(params.room)){
             callback("Name and room are required ");
         }
+
+        // add user to specific room and ensure that messages are sent to that room
+        socket.join(params.room);
+
+        socket.emit("newMessage", generateMessage("Admin", "Welcome to the chat app"));
+        socket.broadcast.to(params.room).emit("newMessage", generateMessage("Admin", ` ${params.name} has joined`));
 
         callback();
     });
